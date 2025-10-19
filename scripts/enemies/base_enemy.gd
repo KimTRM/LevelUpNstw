f8252f8a6bf6d3d1ae9114127c738f2bbd2e9d9d
Kim Louise <kimlabrador71@gmail.com>
2025-10-19 22:59:01 +0800

    Implement player and enemy mechanics, including movement, detection area, and animations
    Enemy AI (Shadow Sprite) Prototye #5

---
+++ scripts/enemies/base_enemy.gd
@@ -0,0 +1,25 @@
+class_name BaseEnemy extends CharacterBody2D
+
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+
+var _direction: Vector2 = Vector2.ZERO
+
+var player_encountered: bool = false
+
+func _physics_process(_delta: float) -> void:
+	if player_encountered:
+		_direction = (get_tree().get_first_node_in_group("Player").global_position - global_position).normalized()
+	else:
+		_direction = Vector2.ZERO
+
+	velocity = velocity_component.get_velocity(_direction.normalized())
+	velocity_component.accelerate_to_velocity(velocity, _delta)
+	velocity_component.move(self)
+
+
+func _on_detection_area_component_body_entered(_body: Node2D) -> void:
+	player_encountered = true
+
+
+func _on_detection_area_component_body_exited(_body: Node2D) -> void:
+	player_encountered = false
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,36 +1,15 @@
 class_name Player
 extends CharacterBody2D
 
-@onready var velocity_component: VelocityComponent = $VelocityComponent
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var sprite: Sprite2D = $Sprite2D
-
 const SPEED = 300.0
 @export var extra_speed: float = 1.0
 
-var _direction: Vector2
-var _animation_direction: String = "down"
-
 func _physics_process(_delta: float) -> void:
-	_direction = Input.get_vector("left", "right", "up", "down")
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
 	
-	velocity = velocity_component.get_velocity(_direction)
-	velocity_component.accelerate_to_velocity(velocity * extra_speed, _delta)
-	velocity_component.move(self)
-	
-	update_animation()
-
-
-func update_animation():
-	if _direction == Vector2.DOWN:
-		_animation_direction = "down"
-	elif _direction == Vector2.UP:
-		_animation_direction = "up"
-	elif _direction == Vector2.LEFT or _direction == Vector2.RIGHT:
-		_animation_direction = "side"
-		sprite.flip_h = _direction.x < 0
-
-	if _direction != Vector2.ZERO:
-		animation_player.play("walk_" + _animation_direction)
+	if direction:
+		velocity = direction * SPEED
 	else:
-		animation_player.play("idle_" + _animation_direction)
+		velocity = Vector2.ZERO
+		
+	move_and_slide()
